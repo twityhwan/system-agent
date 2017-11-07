8b216ad9061ddfc8f0e07963450cd9be02ad3e86
HwanHee Yang <twityhwan@alticast.com>
2017-11-07 14:02:34 +0900

    Added network list and details.

--- src/app/netmanager/net.component.ts
+++ src/app/netmanager/net.component.ts
@@ -8,4 +8,32 @@ import { Component } from '@angular/core';
 
 export class NetComponent {
     title = 'Network Management';
+    public selected: object = undefined;
+    
+    netdevices = [
+        {
+            name: "eth0",
+            interface: "Ethernet",
+            ip: "10.50.1.23",
+            mac: "20:3B:5A:50:2C:B8"
+        },
+        {
+            name: "eth1",
+            interface: "Ethernet",
+            ip: "10.50.1.25",
+            mac: "21:3B:5A:51:2C:B8"
+        },
+        {
+            name: "ALTIWLAN",
+            interface: "Wi-Fi",
+            ip: "10.50.1.105",
+            mac: "21:3C:5A:51:2C:C8"
+        }
+    ];
+
+    netList = {
+        onItemClicked: (item: object) => {
+            this.selected = item;
+        }
+    };
 }
